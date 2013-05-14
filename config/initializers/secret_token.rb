a89838a458d0cc6d30218791936e0a8a87800ab3
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-05-14 21:51:57 +0900

    add initial-files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xva2s3::Application.config.secret_token = 'e245480cafa897cda9f727a2b2178c899ef7b74c99b1e03c055953b9b01f01fb6eae15984837b408ec1c11c7d0775489297f9bb848e86ca7eb79f438e1208608'
